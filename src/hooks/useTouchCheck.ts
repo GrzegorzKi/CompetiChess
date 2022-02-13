7e60f845f5de272fd0ced21137e71cbdae314c57
Grzegorz Kita <grzegorz.kita@protonmail.com>
2022-02-13 15:54:32 +0100

    Replace mobile phone check with matchMedia query
    Using (pointer:coarse) for that goal

--- src/hooks/useTouchCheck.ts
+++ src/hooks/useTouchCheck.ts
@@ -17,24 +17,12 @@
  * along with CompetiChess.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { JSX } from 'preact';
-import { useCallback, useRef } from 'preact/hooks';
+let isTouch: boolean | null = null;
 
-import useTouchCheck from './useTouchCheck';
-
-export default function useContextMenuHandler<T extends EventTarget>(
-  handler: (event: JSX.TargetedMouseEvent<T>) => void): JSX.MouseEventHandler<T> {
-  const lastElementRef = useRef<T>();
-  const isMobile = useTouchCheck();
-
-  return useCallback((event) => {
-    // We don't want to disable native context menu for mobile users
-    // (for example, in case of selecting text)
-    if (lastElementRef.current !== event.currentTarget && !isMobile) {
-      event.preventDefault();
-      lastElementRef.current = event.currentTarget;
-    }
-
-    handler(event);
-  }, [isMobile, handler]);
+export default function useTouchCheck(): boolean {
+  if (isTouch === null) {
+    const match = window.matchMedia('(pointer:coarse)');
+    isTouch = match && match.matches;
+  }
+  return isTouch;
 }
