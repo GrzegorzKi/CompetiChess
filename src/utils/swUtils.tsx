b5c9b233aacc292b3708a768ce69bfc4721075cc
Grzegorz Kita <grzegorz.kita@protonmail.com>
2022-06-08 20:54:09 +0200

    Create notification when ServiceWorker is fully installed first time

--- src/utils/swUtils.tsx
+++ src/utils/swUtils.tsx
@@ -17,6 +17,8 @@
  * along with CompetiChess.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { faWifi } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { h } from 'preact';
 import { Trans } from 'react-i18next';
 import { toast } from 'react-toastify';
@@ -25,18 +27,29 @@ import { toast } from 'react-toastify';
 export function listenToSwUpdates(): void {
   if ('serviceWorker' in navigator) {
     let refreshing = false;
+    const swFirstInstalled = !navigator.serviceWorker.controller;
 
     // This fires when the service worker controlling this page
     // changes, eg a new worker has skipped waiting and become
     // the new active worker.
     navigator.serviceWorker.addEventListener('controllerchange', () => {
       if (!refreshing) {
-        window.location.reload();
         refreshing = true;
+        window.location.reload();
       }
     });
 
     navigator.serviceWorker.ready.then(registration => {
+      if (swFirstInstalled) {
+        toast.success((<p>
+          <Trans i18nKey="Application installed">
+            Application has been installed and is ready for offline use!
+          </Trans>
+        </p>), {
+          icon: <Icon icon={faWifi} color="#07aa0c" />
+        });
+      }
+
       registration.addEventListener('updatefound', () => {
         registration.installing?.addEventListener('statechange', () => {
           const waitingWorker = registration.waiting;
